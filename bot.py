9980344f8d5ec6b2049ae17634d861fc6c6fe39b
surik1993 <163700761+surik1993@users.noreply.github.com>
2024-04-09 15:23:15 +1200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,6 +1,21 @@
+# Установим необходимые библиотеки
+#!pip install aiosqlite
+#!pip install aiogram
+# Установим библиотеку nest_asyncio
+#!pip install nest_asyncio
+import nest_asyncio
+nest_asyncio.apply()
+
 #@title Полный код бота для самоконтроля
 
+import aiosqlite
 
+
+# Запускаем создание таблицы базы данных
+#await create_table()
+
+
+import os
 import aiosqlite
 import asyncio
 import logging
@@ -19,15 +34,34 @@ API_TOKEN = '6806372479:AAHTiIRPU6zFdXlZhpm9mg4bW-e_3kHe3p8'
 # Зададим имя базы данных
 DB_NAME = 'quiz_bot.db'
 
-DICT_DATA = 'quiz_data.json'
+DICT_DATA = 'data/quiz_data.json'
+
+
 
 # Объект бота
 bot = Bot(token=API_TOKEN)
 # Диспетчер
 dp = Dispatcher()
 
-with open(DICT_DATA, 'r') as j:
-    quiz_data = json.loads(j.read())
+if not os.path.exists(DICT_DATA):
+    print(f"The file '{DICT_DATA}' не существует. Пожалуйста создайте его или загрузите из надежного источника.")
+else:
+    #with open(DICT_DATA, 'r') as j:
+        #quiz_data = json.loads(j.read())
+    with open(DICT_DATA, 'r', encoding='utf-8') as j:
+        quiz_data = json.loads(j.read())
+
+#with open(data, 'r') as file:
+    #quiz_data = json.load(file)
+
+#for question in quiz_data:
+    #print(f"Question: {question['question']}")
+    #print(f"Options: {question['options']}")
+    #print()
+
+
+#with open(DICT_DATA, 'r') as j:
+    #quiz_data = json.loads(j.read())
 
 def generate_options_keyboard(answer_options, right_answer):
     builder = InlineKeyboardBuilder()
@@ -109,14 +143,20 @@ async def get_question(message, user_id):
     await message.answer(f"{quiz_data[current_question_index]['question']}", reply_markup=kb)
 
 
-async def new_quiz(message):
-    user_id = message.from_user.id
+#async def new_quiz(message):
+    #user_id = message.from_user.id
+    #current_question_index = 0
+    #new_score = 0
+    #await update_quiz_index(user_id, current_question_index)
+    #await update_user_score(callback.from_user.id, current_score)
+    #await get_question(message, user_id)
+
+async def new_quiz(callback):
+    user_id = callback.from_user.id
     current_question_index = 0
     new_score = 0
     await update_quiz_index(user_id, current_question_index)
-    await update_user_score(callback.from_user.id, current_score)
-    await get_question(message, user_id)
-
+    await update_user_score(callback, new_score)
 
 async def get_quiz_index(user_id):
      # Подключаемся к базе данных
@@ -129,7 +169,7 @@ async def get_quiz_index(user_id):
                 return results[0]
             else:
                 return 0
-            
+
 async def get_user_score(user_id):
     async with aiosqlite.connect(DB_NAME) as db:
         async with db.execute('SELECT score FROM users WHERE user_id = ?', (user_id,)) as cursor:
@@ -137,9 +177,9 @@ async def get_user_score(user_id):
             if results is not None:
                  return results[0]
             else:
-                 return 0 
-                   
-            
+                 return 0
+
+
 async def update_quiz_index(user_id, index):
     # Создаем соединение с базой данных (если она не существует, она будет создана)
     async with aiosqlite.connect(DB_NAME) as db:
@@ -147,13 +187,18 @@ async def update_quiz_index(user_id, index):
         await db.execute('INSERT OR REPLACE INTO quiz_state (user_id, question_index) VALUES (?, ?)', (user_id, index))
         # Сохраняем изменения
         await db.commit()
-        
-async def update_user_score(user_id, new_score):
 
+#async def update_user_score(user_id, new_score):
+
+    #async with aiosqlite.connect(DB_NAME) as db:
+        #await db.execute('INSERT INTO users (user_id, score) VALUES (?,?) ON CONFLICT (user_id) DO UPDATE SET score = excluded.score', (user_id, new_score))
+        #await db.commit()
+
+async def update_user_score(callback, new_score):
+    user_id = callback.from_user.id
     async with aiosqlite.connect(DB_NAME) as db:
         await db.execute('INSERT INTO users (user_id, score) VALUES (?,?) ON CONFLICT (user_id) DO UPDATE SET score = excluded.score', (user_id, new_score))
         await db.commit()
-
 
 # Хэндлер на команду /quiz
 @dp.message(F.text=="Начать игру")
@@ -161,16 +206,15 @@ async def update_user_score(user_id, new_score):
 async def cmd_quiz(message: types.Message):
 
     await message.answer(f"Давайте начнем квиз!")
+    #await new_quiz(message)
     await new_quiz(message)
-
-
 
 async def create_table():
     # Создаем соединение с базой данных (если она не существует, она будет создана)
     async with aiosqlite.connect(DB_NAME) as db:
         # Создаем таблицу
         await db.execute('''CREATE TABLE IF NOT EXISTS quiz_state (user_id INTEGER PRIMARY KEY, question_index INTEGER)''')
-        await db.execute('''CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY? score INTEGER)''')
+        await db.execute('''CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, score INTEGER)''')
         # Сохраняем изменения
         await db.commit()
 
@@ -178,7 +222,7 @@ async def create_table():
 @dp.message(Command("help"))
 async def cmd_start(message:types.Message):
     await message.answer('Команды бота: \n\start - начать взаимодействие с ботом\n\help - открыть помощь\n\quiz - начать игру')
-    
+
 # Запуск процесса поллинга новых апдейтов
 async def main():
 
